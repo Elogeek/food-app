52ca640b468801170178855608c26ed6fb291ec8
elogeek <elodiechristin@gmail.com>
2022-09-06 12:47:47 +0200

    suite project

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -3,5 +3,5 @@ export const getRecipesList = store => store.recipes.list;
 export const getSelectedRecipe = store => store.recipes.selectedRecipe;
 
 // Task list
-export const getTasks = store => store.tasksList;
-export const getCompletedTasks = store => store.tasksList.filter(task => task.isCompleted);
+//export const getTasks = store => store.tasksList;
+//export const getCompletedTasks = store => store.tasksList.filter(task => task.isCompleted);
